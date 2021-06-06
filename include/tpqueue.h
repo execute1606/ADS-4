3f63226ee42b0d8d75228f82d8a15f25a0c5c4c3
execute1606 <72761350+execute1606@users.noreply.github.com>
2021-06-06 23:10:40 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,12 +5,86 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+private:
+    T* arr;
+    int size;
+    int begin;
+    int end;
+    int count;
+public:
+    explicit TPQueue(int = 100);
+    ~TPQueue();
+    void push(const T&);
+
+    T pop();
+    T get() const;
+    bool Full() const;
+    bool Empty() const;
 };
 
+template<typename T>
+TPQueue<T>::TPQueue(int sizeQueue) :
+    size(sizeQueue),
+    begin(0), end(0), count(0) {
+    arr = new T[size + 1];
+}
+
+template<typename T>
+TPQueue<T>::~TPQueue() {
+    delete[] arr;
+}
+
+template<typename T>
+void TPQueue<T>::push(const T& item) {
+    assert(count > size);
+    if (end != 0) {
+        arr[end] = item;
+        for (int i = end - 1; i >= begin; i--) {
+            if (arr[i].prior < item.prior) {
+                arr[i] = item;
+                arr[i + 1] = arr[i];
+            }
+        }
+    }
+    else {
+        arr[begin] = item;
+    }
+    end++;
+    count++;
+    if (end > size) {
+        end -= size + 1;
+    }
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+    assert(count > 0);
+    T item = arr[begin++];
+    count--;
+    if (begin > size)
+        begin -= size + 1;
+    return item;
+}
+
+template<typename T>
+T TPQueue<T>::get() const {
+    assert(count > 0);
+    return arr[begin];
+}
+
+template<typename T>
+bool TPQueue<T>::Empty() const {
+    return count == 0;
+}
+
+template<typename T>
+bool TPQueue<T>::Full() const {
+    return count == size;
+}
+
 struct SYM {
-  char ch;
-  int  prior;
+    char ch;
+    int  prior;
 };
 
 #endif // INCLUDE_TPQUEUE_H_
